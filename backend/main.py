f9de06dcd10bc47a34d0fb8dbd42ff4a9da988a5
Samuel Santos <samuka0102128@gmail.com>
2025-10-25 17:51:21 -0300

    Model de Usuario e banco de dados criados

---
+++ backend/main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from models.db import db
+from models.Usuario import Usuario
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
+db.init_app(app)
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
---
+++ backend/models/Usuario.py
@@ -0,0 +1,10 @@
+from models.db import db
+
+class Usuario(db.Model):
+    __tablename__ = 'usuarios'
+    
+    id = db.Column(db.Integer, primary_key = True)
+    
+    nome = db.Column(db.String(30), unique= True)
+    senha = db.Column(db.String())
+    
