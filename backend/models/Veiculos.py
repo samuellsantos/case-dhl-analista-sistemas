ef6f44836bd017cb09b44719132a13a7db3911b2
Samuel Santos <samuka0102128@gmail.com>
2025-10-26 16:40:46 -0300

    Rota de registro de veículos

--- backend/models/Veiculos.py
+++ backend/models/Veiculos.py
@@ -1,15 +1,15 @@
-from db import db
 from datetime import datetime
-from zoneinfo import ZoneInfo
+import pytz
+from db import db
 
 class Veiculos(db.Model):
     __tablename__ = 'veiculos'
     
-    id = db.Column(db.Integer, primary_key = True)
-    nome_motorista = db.Column(db.String(30), nullable = False)
-    transportadora = db.Column(db.String(30), nullable = False)
-    placa = db.Column(db.String(30), nullable = False)
+    id = db.Column(db.Integer, primary_key=True)
+    nome_motorista = db.Column(db.String(30), nullable=False)
+    transportadora = db.Column(db.String(30), nullable=False)
+    placa = db.Column(db.String(30), nullable=False)
     observacoes = db.Column(db.String(50))
-    status = db.Column(db.String(30), nullable = False)
-    dt_entrada = db.Column(db.DateTime, default=lambda: datetime.now(ZoneInfo("America/Sao_Paulo")))
+    status = db.Column(db.String(30), nullable=False)
+    dt_entrada = db.Column(db.DateTime, default=lambda: datetime.now(pytz.timezone("America/Sao_Paulo")))
     dt_saida = db.Column(db.DateTime)
--- backend/routes/vehicles_route.py
+++ backend/routes/vehicles_route.py
@@ -6,6 +6,29 @@ from db import db
 vehicles_bp = Blueprint('vehicles', __name__)
 
 
-@vehicles_bp.route('/')
-def home():
-    return jsonify({'message': 'OK'})
+@vehicles_bp.route('/registrar_veiculo', methods = ['POST'])
+@login_required
+def registrar_veiculo():
+    data = request.json
+    nome_motorista = data.get('nome_motorista')
+    transportadora = data.get('transportadora')
+    placa = data.get('placa')
+    observacoes = data.get('observacoes')
+    status = 'Em Patio'
+    
+    try:
+        registro_veiculo = Veiculos(
+            nome_motorista = nome_motorista,
+            transportadora = transportadora,
+            placa = placa,
+            observacoes = observacoes,
+            status = status
+        )
+        
+        db.session.add(registro_veiculo)
+        db.session.commit()
+        
+        return jsonify({'message': 'Veiculo registrado com sucesso.'}), 201
+    except Exception as e:
+        return jsonify({'message': f'Não foi possível registrar o veiculo. {e}'}), 401
+    
